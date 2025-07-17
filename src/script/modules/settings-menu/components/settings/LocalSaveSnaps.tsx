5dc133cf4ed3d0ed3c2776b48704720c623a6e0c
Jaxson Waayers <79427951+mixtapejaxson@users.noreply.github.com>
2025-07-16 18:15:05 -0700

    Delete src/script/modules/settings-menu/components/settings/LocalSaveSnaps.tsx

--- src/script/modules/settings-menu/components/settings/LocalSaveSnaps.tsx
+++
@@ -1,8 +0,0 @@
-import { LocalSaveSnaps } from '../../../local-save-snaps/index';
-
-const localSaveSnapsModule = new LocalSaveSnaps();
-
-export default {
-    name: localSaveSnapsModule.name,
-    component: localSaveSnapsModule.getSettingsComponent(),
-};
